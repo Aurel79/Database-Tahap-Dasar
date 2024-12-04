305a5722daf6cd8dfb92a56a46853bb7a277e3fe
Aurel79 <faurelia795@gmail.com>
2024-12-05 00:00:57 +0700

    Database Tahap Dasar
    Dibuat dengan project Laravel

---
+++ laravel-controller-view - Copy/routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// home kiri itu untuk akses url dan yang kanan untuk navbar yang ada di file home.blade.php/
+Route::get('home', [HomeController::class, 'index'])->name('home');
+Route::get('profile', ProfileController::class)->name('profile');
+Route::resource('employees', EmployeeController::class);
